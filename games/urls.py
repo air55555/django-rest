8b033f006aa1fd8f38805f30802862415901be4f
Tim White <twhite@fool.com>
2021-05-10 09:02:14 -0600

    Updating url comment.

--- games/urls.py
+++ games/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path("", RedirectView.as_view(url="/games/api/")),
     path("games/api/", include(games_api_router.urls)),
     # Automatically Created API URLs from the Router:
-    # api/ - list of all games (url name is 'game-list')
-    # api/<pk> - detail of a single game based on its primary key (url name is 'game-detail')
+    # /games/api/ - list of all games (url name is 'game-list')
+    # /games/api/<pk> - detail of a single game based on its primary key (url name is 'game-detail')
 ]
